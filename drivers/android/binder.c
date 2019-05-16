1f227abaa002ef1a9a511b299eca4e77bb22b571
shagbag913 <sh4gbag913@gmail.com>
2019-05-16 17:05:03 -0400

    binder: move to the "real" part of the kernel
    This is a squash of a couple of commits, however the biggest is
    obviously moving the binder driver. This was all done by hand to
    preserve existing binder changes.
    
    Upstream did this during Linux ~3.19
    
    Signed-off-by: shagbag913 <sh4gbag913@gmail.com>
    Signed-off-by: Kai Jones <joneskai626@gmail.com>
    Change-Id: I76fbd26675ff3ab0e5bb5a9d8ac4edb4d3055cf5
    Signed-off-by: Lunarixus <Nathanb644@hotmail.co.uk>

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -49,7 +49,12 @@
 #include <linux/prio_tracer.h>
 #endif
 
-#include "binder.h"
+#ifdef CONFIG_ANDROID_BINDER_IPC_32BIT
+#define BINDER_IPC_32BIT 1
+#endif
+
+#include <uapi/linux/android/binder.h>
+#include "binder_alloc.h"
 #include "binder_trace.h"
 
 static DEFINE_RT_MUTEX(binder_main_lock);
